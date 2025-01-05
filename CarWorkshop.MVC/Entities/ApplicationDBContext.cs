c6d4caea4bd351094017e333c1973720a09c175f
pleanna <74422085+pleanna@users.noreply.github.com>
2025-01-05 17:41:28 +0100

    Dodaj pliki projektów.

---
+++ CarWorkshop.MVC/Entities/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CarWorkshop.MVC.Entities
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<CarWorkshopEntity> CarWorkshops { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb;Database = CarWorkshopMVC;Trusted_Connection = True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
